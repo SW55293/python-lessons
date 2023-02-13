12ceabd7ea384f0970c817890865252ee4e0cd5a
Stephanie <yesstephanie1@gmail.com>
2023-02-12 20:05:30 -0800

    linked list cycle

--- leetcode/easy/linked_list/linked_list_cycle.py
+++ leetcode/easy/linked_list/linked_list_cycle.py
@@ -12,10 +12,30 @@ class Solution(object):
         slow_pointer = head
         fast_pointer = head
 
-        while fast_pointer and fast_pointer.next:
+        while fast_pointer!=None and fast_pointer.next!=None:
             fast_pointer = fast_pointer.next.next
             slow_pointer = slow_pointer.next
-            
+
             if fast_pointer is slow_pointer:
                 return True
         return False
+
+'''
+Another way (much faster)
+
+def hasCycle(self,head):
+    slow = head
+    fast = head.next
+
+    while slow is not fast:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow is fast:
+        return True
+    return False
+
+'''
+
+# Time:  O(n) : depends on the size of the linked list which is n
+# Space: O(1) : No extra data structures are added, we do all the lookups using whats already there
