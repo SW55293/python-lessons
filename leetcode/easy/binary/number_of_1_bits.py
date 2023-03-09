f328a6b61f263f08a974da1a1e96fd159b2bdd52
Stephanie <yesstephanie1@gmail.com>
2023-03-08 21:23:28 -0800

    extra

--- leetcode/easy/binary/number_of_1_bits.py
+++ leetcode/easy/binary/number_of_1_bits.py
@@ -36,3 +36,25 @@ So since 2 x 5 = 10 that is the closest we can get to 11 without going over
 we subtract it from the pre-mod number and the answer is the remaider
 11 - 10 = 1
 '''
+
+'''
+Example
+Input: n = 00000000000000000000000000001011
+Output: 3
+Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+
+
+we mod every single number in n the input. We know all 0 mod 2 will give us a zero. 
+We also know that if we see a 1 and mod it by 2 we get 1 remainder
+
+1 mod 2 
+multiples of 2 = 0,2,4,6..
+(we are going to use 2x0=0, because thats the closest we get to 1 wihtout going over)
+so..
+
+1 - 0 = 1
+So our answer to 1 mod 2 = 1
+
+every time we mod 2 a 1 we get 1 and add it to the counter. We then shift right to continue
+down the input running the same formula
+'''
