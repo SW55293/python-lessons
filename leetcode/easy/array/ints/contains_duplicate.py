c40516bde64b8cac1d58fda3ebfef6b55f6084fb
Stephanie <yesstephanie1@gmail.com>
2023-03-20 21:34:35 -0700

    stuff

--- leetcode/easy/array/ints/contains_duplicate.py
+++ leetcode/easy/array/ints/contains_duplicate.py
@@ -11,12 +11,13 @@ def containsDuplicate(nums):
 
     dict = {}
 
-    for x in range(len(nums)):
+    for x in nums:
         if nums[x] in dict:
             return True
         dict[nums[x]] = x
 
     return False
+print(containsDuplicate([1,2,3,3]))
 
 # Pattern          = Sort and Compare Length
 # Time Complexity  = O(n)
@@ -25,10 +26,10 @@ def containsDuplicate(nums):
 def containsDuplicate2(nums):
     nums.sort()
 
-    for i in range(len(nums)-1):
+    for i in range(len(nums)-1):   # or just for i in nums: works
         if nums[i] == nums[i+1]: return True
     return False
-
+print(containsDuplicate2([1,2,3,3]))
 
 # Pattern          = Set & Len Function Comparing
 # Time Complexity  = O(n)
