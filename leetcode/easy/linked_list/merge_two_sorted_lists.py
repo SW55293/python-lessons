aeeca1f5bd880b6304c4b1c006b3bb0f1a208494
Stephanie <yesstephanie1@gmail.com>
2023-03-26 21:39:46 -0700

    lists

--- leetcode/easy/linked_list/merge_two_sorted_lists.py
+++ leetcode/easy/linked_list/merge_two_sorted_lists.py
@@ -1,6 +1,6 @@
 # Pattern          = Two Pointer
 # Time Complexity  = O(n)
-# Space Complexity = O(1)
+# Space Complexity = O(n)
 """
 Input1 = Singly Linked List
 Return type = Singly Linked List
@@ -11,28 +11,32 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
 
         # Create an empty ListNode
         dummy_list = ListNode()
-        new_head = dummy_list
+        dummy_head = dummy_list  #have it point to the only node in the list (which is zero)
      
+     #we iterate till one or the other is not empty
         while list1 and list2:   
             if list1.val < list2.val:
-                new_head.next = list1
+                dummy_head.next = list1
                 list1 = list1.next
             else:
-                new_head.next = list2
+                dummy_head.next = list2
                 list2 = list2.next
-            new_head = new_head.next
+
+              #increment the to the next node  
+            dummy_head = dummy_head.next
 
         # covering the cases when list1 or list2 end up empty before the other
+        #if one does then just have the new lists next pointer point to the rest of that list
         if list1:
-            new_head.next = list1
+            dummy_head.next = list1
         if list2:
-            new_head.next = list2
+            dummy_head.next = list2
         
 
         return dummy_list.next
