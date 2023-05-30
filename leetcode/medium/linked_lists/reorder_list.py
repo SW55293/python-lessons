b685b02966c3e45e08ffcdbfd805a2de54d272dd
Stephanie <yesstephanie1@gmail.com>
2023-05-29 21:06:59 -0700

    graph - medium

--- leetcode/medium/linked_lists/reorder_list.py
+++ leetcode/medium/linked_lists/reorder_list.py
@@ -1,5 +1,55 @@
-
 def reorderList(head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        '''
+        The function works by first using two pointers, slow and fast, to find the middle of the linked list. 
+        The slow pointer moves one node at a time, while the fast pointer moves two nodes at a time. When the fast pointer 
+        reaches the end of the linked list, the slow pointer will be pointing to the middle node.
+        '''
+        # finds the middle and the last node
+        slow = head 
+        fast = head.next
+        #while fast and fast.next != None
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Separates the 2 halfs 
+        start_of_second = slow.next
+        slow.next = None
+        prev = None
+        while start_of_second:
+            temp = start_of_second.next
+            start_of_second.next = prev
+            prev = start_of_second
+            start_of_second = temp
+
+        # Reverses the next pointers of the 2 halfs and shifts the nodes around 
+        first = head
+        second = prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
+    
+
+
+
+
+
+
+
+
+
+
+
+
+def reorder_List(head):
     """
     Do not return anything, modify head in-place instead.
     """
@@ -47,46 +97,12 @@ def reorderList(head):
 
 
 '''
-head=ListNode()
-class Solution:
-  def reorderList(self, head: ListNode) -> None:
-    def findMid(head: ListNode):
-      prev = None
-      slow = head
-      fast = head
-
-      while fast and fast.next:
-        prev = slow
-        slow = slow.next
-        fast = fast.next.next
-      prev.next = None
-
-      return slow
-
-    def reverse(head: ListNode) -> ListNode:
-      prev = None
-      curr = head
-
-      while curr:
-        next = curr.next
-        curr.next = prev
-        prev = curr
-        curr = next
-
-      return prev
-
-    def merge(l1: ListNode, l2: ListNode) -> None:
-      while l2:
-        next = l1.next
-        l1.next = l2
-        l1 = l2
-        l2 = next
-
-    if not head or not head.next:
-      return
-
-    mid = findMid(head)
-    reversed = reverse(mid)
-    merge(head, reversed)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    
 
 '''
