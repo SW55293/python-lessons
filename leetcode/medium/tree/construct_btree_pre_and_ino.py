9dc9f0146318b53dfd3705751a7b2720817b832e
Stephanie <yesstephanie1@gmail.com>
2023-06-03 21:38:33 -0700

    array int - medium

--- leetcode/medium/tree/construct_btree_pre_and_ino.py
+++ leetcode/medium/tree/construct_btree_pre_and_ino.py
@@ -15,9 +15,11 @@ def buildTree(self, preorder, inorder):
 
         #start of binary tree
         root = TreeNode(preorder[0])
-        #everything on the right of the partition is the right side of tree\
+        #everything on the right of the partition is the right side of the tree
         #while everything to the left is the left side of the tree
         partition = inorder.index(preorder[0])
+        
         root.left = self.buildTree(preorder[1 : partition + 1], inorder[:partition])
         root.right = self.buildTree(preorder[partition + 1 :], inorder[partition + 1 :])
         return root
+
