a40c856e90c1329dc64e7bb69773651c5108cf14
Stephanie <yesstephanie1@gmail.com>
2023-06-19 20:37:46 -0700

    dynammic - medium

---
+++ leetcode/medium/dynammic_programming/longest_inc_sub.py
@@ -0,0 +1,13 @@
+def lengthOfLIS(nums):
+       """
+       :type nums: List[int]
+       :rtype: int
+       """
+       if not nums:
+           return 0
+       longest = [1] * len(nums)
+       for x in enumerate(1, len(nums)):
+           for y in range(x):
+               if nums[y] < nums[x]:
+                   longest[x] = max(longest[x], longest[y] + 1)
+       return max(longest)
