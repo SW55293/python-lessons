669b6312fa4b718cf59287f7c3f0e0456c35dcdd
Stephanie <yesstephanie1@gmail.com>
2023-09-19 20:56:14 -0700

    tree - medium

--- leetcode/medium/tree/implement_trie.py
+++ leetcode/medium/tree/implement_trie.py
@@ -35,3 +35,88 @@ class Trie:
                 return False
             current = current.children[i]
         return True
+'''
+Commented code
+'''
+
+class TrieNode:
+    def __init__(self):
+        # Initialize a TrieNode with 26 children, one for each lowercase English letter (a to z).
+        self.children = [None] * 26
+        # 'end' is a boolean flag that indicates whether a word ends at this node.
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        # Initialize a Trie with a root node.
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        # Iterate through each character in the input word.
+        for c in word:
+            # Calculate the index of the character in the 'children' list using ASCII values.
+            i = ord(c) - ord("a")
+            # If the child node for the character doesn't exist, create one.
+            if current.children[i] is None:
+                current.children[i] = TrieNode()
+            # Move to the child node and continue the loop.
+            current = current.children[i]
+        # Mark the last node as the end of a word.
+        current.end = True
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        # Iterate through each character in the input word.
+        for c in word:
+            i = ord(c) - ord("a")
+            # If the child node for the character doesn't exist, the word is not in the Trie.
+            if current.children[i] is None:
+                return False
+            # Move to the child node and continue the loop.
+            current = current.children[i]
+        # If we've reached the end of the word and 'end' is True, the word is in the Trie.
+        return current.end
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        # Iterate through each character in the input prefix.
+        for c in prefix:
+            i = ord(c) - ord("a")
+            # If the child node for the character doesn't exist, the prefix is not in the Trie.
+            if current.children[i] is None:
+                return False
+            # Move to the child node and continue the loop.
+            current = current.children[i]
+        # If we've reached the end of the prefix, it means the Trie contains words with that prefix.
+        return True
+
+'''
+Bard Explanation
+
+The code you provided is a Python implementation of a trie data structure. A trie is a tree-like data structure that is used to store strings efficiently. Each node in a trie represents a single character, and the children of a node represent all of the possible characters that can follow that character.
+
+The `TrieNode` class represents a single node in a trie. It has two attributes:
+
+* `children`: A list of 26 `TrieNode` objects, representing all of the possible children of the node. The index of a child node in the list corresponds to the character that it represents.
+* `end`: A boolean value indicating whether or not the node represents the end of a word.
+
+The `Trie` class represents a trie data structure as a whole. It has a single attribute:
+
+* `root`: A `TrieNode` object representing the root node of the trie.
+
+The `insert()` method inserts a word into the trie. It does this by iterating over the characters in the word and creating a new `TrieNode` object for each character, if necessary. The `insert()` method also sets the `end` attribute of the leaf node to `True` to indicate that it represents the end of the word.
+
+The `search()` method searches for a word in the trie. It does this by iterating over the characters in the word and following the corresponding child nodes in the trie. If the `search()` method reaches a leaf node and the `end` attribute is set to `True`, then the word is found. Otherwise, the word is not found.
+
+The `startsWith()` method checks whether or not a given string is a prefix of any word in the trie. It does this by iterating over the characters in the prefix and following the corresponding child nodes in the trie. If the `startsWith()` method reaches the end of the prefix, then the prefix is a prefix of at least one word in the trie. Otherwise, the prefix is not a prefix of any word in the trie.
+
+Here are some additional comments on the code:
+
+* The `TrieNode` class could also have an additional attribute to store the character that the node represents. This would be useful for debugging purposes, but it is not necessary for the trie to function.
+* The `Trie` class could also have additional methods, such as a method to delete a word from the trie or a method to get a list of all of the words in the trie.
+* The `insert()` method could be optimized to avoid creating unnecessary `TrieNode` objects. For example, if the trie already contains a node for the first character of the word, then the `insert()` method could simply reuse that node.
+* The `search()` and `startsWith()` methods could be optimized to avoid traversing the entire trie. For example, if the trie contains a node for the prefix of the word or string, then the `search()` or `startsWith()` method could start its traversal from that node.
+
+Overall, the code you provided is a well-written and efficient implementation of a trie data structure.
+'''
