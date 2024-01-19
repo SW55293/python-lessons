100108515cdafd82ae7daae0744c3e4758b939ab
Stephanie <yesstephanie1@gmail.com>
2024-01-18 21:28:59 -0800

    linked lists

--- data-structures/linked_lists/singly/single.py
+++ data-structures/linked_lists/singly/single.py
@@ -1,8 +1,28 @@
 class Node:
-    def __init__(self, val=None, next=None) -> None:
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
-class LinkedList:
-    def __init__(self, head) -> None:
+class LinkedList(Node):
+    def __init__(self, head):
         self.head = head
+
+    def printLinkedList(self): 
+            temp = self.head 
+            while (temp): 
+                print (temp.data, " -> ", end = '') 
+                temp = temp.next_node
+            print("")
+
+    def addNode(self, new_node):
+        curr = self.head
+
+        while curr and curr.next: 
+              curr = curr.next
+        curr.next = new_node
+        
+    
+
+
+
+     
