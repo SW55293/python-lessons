fd69685b1ca9782c308fb0dac710dce7e2ddee9c
Stephanie <yesstephanie1@gmail.com>
2024-02-02 22:01:44 -0800

    tree

--- data-structures/trees/tree.py
+++ data-structures/trees/tree.py
@@ -1,3 +1,47 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def insert(self, root, val):
+        # This places any node into an empty null/none spot
+        if root is None:
+            return Node(val)  # Return the new node directly
+
+        # If the value is less
+        if val < root.val:
+            root.left = self.insert(root.left, val)
+        else:
+            root.right = self.insert(root.right, val)
+        return root  # Return the updated root
+
+    def inorder_traversal(self, root):
+        if root:
+            self.inorder_traversal(root.left)
+            print(root.val, end=" ")
+            self.inorder_traversal(root.right)
+
+# Root is not part of the node class, you have to define it outside of the class
+root = Node(12)
+
+# Use the insert method to add nodes
+root.insert(root, 6)  # root.left
+root.insert(root, 14)  # root.right
+root.insert(root, 3)  # root.left.left
+root.insert(root,18)
+
+root.inorder_traversal(root)
+
+
+
+
+
+
+
+
+
+'''
 # binary tree node class
 
 class Node:
@@ -11,8 +55,8 @@ class Node:
    def insert(self, root, val):
       #this places any node into an empyt null/none spot
       if root is None:
-         root = Node(val)
-         return root
+         return Node(val)
+      
       
       # If the value is less
       if val < root.val:
@@ -38,8 +82,9 @@ class Node:
 root = Node(12)
 
 # Use the insert method to add nodes
-root.insert(6) #root.left
-root.insert(14) #root.right
-root.insert(3) #root.left
+root.insert(root, 6) #root.left
+root.insert(root, 14) #root.right
+root.insert(root, 3) #root.left
 
 root.PrintTree()
+'''
