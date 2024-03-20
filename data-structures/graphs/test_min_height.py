6d23a86deeac9a90ba997f687889d40f03159469
Stephanie <yesstephanie1@gmail.com>
2024-03-19 22:24:49 -0700

    graphs

--- data-structures/graphs/test_min_height.py
+++ data-structures/graphs/test_min_height.py
@@ -7,40 +7,63 @@ class Solution:
         if n <= 2:
             return [i for i in range(n)] #returns the values from 0 to n
 
-        # represent the nodes and edges in a graph using a hashmap
-        # [0 -> 1, 2]
-        # [1 -> 0]
-        # ...etc
-        # this creates a dictionary with nodes on one column from 0 to n
-        # and a list of all the other nodes they are connected to
-        graph = {num_nodes: [] for num_nodes in range(n)}
+       
+        neighbors = [set() for _ in range(n)]
+        for u, v in edges:
+            neighbors[u].add(v)
+            neighbors[v].add(u)
 
-        for node1, node2 in edges:
-            graph[node1].append(node2)
-            graph[node2].append(node1)
+        # Find initial leaf nodes
+        leaves = [i for i in range(n) if len(neighbors[i]) == 1]
 
-        # populate the first leaf layer
-        leaves = []
-        for node in graph:
-            # leaves will only have one connection
-            if len(graph[node]) == 1:
-                leaves.append(node)
+        # Repeatedly remove leaves layer by layer
+        remaining_nodes = n
+        while remaining_nodes > 2:
+            remaining_nodes -= len(leaves)
+            new_leaves = []
 
-        # outer while loop runs until only two nodes left as leaves are removed
-        while n > 2:
-            n -= len(leaves)
-            # store next layer of leaves which will be constructed in inner while loop
-            next_leaves_layer = []
-            while leaves:
-                leaf = leaves.pop()
-                # 1. remove leaf-neighbour edges
-                neighbour = graph[leaf].pop()
-                neighbour_connections = graph[neighbour]
-                neighbour_connections.remove(leaf)
-                # 2. create new leaf layer from neighbour
-                if len(neighbour_connections) == 1:
-                    next_leaves_layer.append(neighbour)
-            # reassign next layer to 'leaves'
-            leaves = next_leaves_layer
-        
-        return leaves
+            for leaf in leaves:
+                temp = neighbors[leaf].pop()  # The only neighbor of the leaf
+                neighbors[temp].remove(leaf) #remove the leaf that was popped from the dict/list of nodes
+
+                if len(neighbors[temp]) == 1:
+                    new_leaves.append(temp)
+            leaves = new_leaves
+
+        return leaves  # The remaining nodes are the roots of MHTs
+   
+n = 4
+edges = [[1,0],[1,2],[1,3]]
+sol = Solution()
+
+answer = sol.findMinHeightTrees(n, edges)
+print(answer)
+
+'''
+Example output for neighbors and for loop code
+
+>>> n = 4
+>>> edges = [[1,0],[1,2],[1,3]]
+>>> neighbors = [set() for _ in range(n)]
+>>> for u, v in edges:
+...     neighbors[u].add(v)
+...     neighbors[v].add(u)
+... 
+>>> print(neighbors)
+[{1}, {0, 2, 3}, {1}, {1}]
+>>> 
+
+'''
+#graph = {x:[] for x in range(n)}
+#for u, v in edges:
+#    neighbors[u].append(v)
+#    neighbors[v].append(u)
+# represent the nodes and edges in a graph using a hashmap
+# [0 -> 1, 2]
+# [1 -> 0]
+# ...etc
+# this creates a dictionary with nodes on one column from 0 to n
+# and a list of all the other nodes they are connected to
+
+
+
