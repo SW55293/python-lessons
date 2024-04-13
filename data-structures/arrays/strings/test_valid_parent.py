4bd0ed0784f6f024b86ca578572f1abd6fa057e3
Stephanie <yesstephanie1@gmail.com>
2024-04-12 22:34:46 -0700

    new questions

--- data-structures/arrays/strings/test_valid_parent.py
+++ data-structures/arrays/strings/test_valid_parent.py
@@ -1,26 +1,25 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        # key is the closing paren
+        all_pairs = {
+            '(':')',
+            '{':'}',
+            '[':']'
+        }
         for x in s:
-            if x == '(':
-                stack.append('(')
-                stack.append(')')
-            elif x == '{':
-                stack.append('{')
-                stack.append('}')
-            elif x == '[':
-                stack.append('[')
-                stack.append(']')
-        if set(s) != set(stack):
-            return False
-        else:
-            return True
+            # if a opening paren is found add to stack
+            if x in all_pairs:
+                 stack.append(x)
+            # if the len of stack is 0 or if the closing bracket doesnt match with the last opening value
+            # added to the stack when using the all_pairs matching map then return false
+            elif len(stack) == 0 or x != all_pairs[stack.pop()]:
+                 return False
 
-
-
+        return len(stack) == 0
 
 sol = Solution()
-s = "([)]"
+s = "(&[)]"
 print(sol.isValid(s))
 
 
